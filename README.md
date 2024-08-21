8002596a142f6fcbbce2c8a9e08386091522ba7d
tylxr <102394635+tylxr59@users.noreply.github.com>
2024-08-20 22:27:11 -0400

    v2.1
    Further cleanup. Still needs a bit more work. I know it can be better, I feel it in my bones.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # tarotjs
-
 A simple tarot card picker in Javascript
 
+Code licensed under MIT License - https://opensource.org/licenses/MIT
+
 Rider-Waite-Smith Card Scans by Steve - https://steve-p.org/cards/RWSa.html (High res versions available at his website)
+
+See it in action - https://tylxr.com/tarot/
+
+**To-Do:**
+* Clean up the code further and turn it into a useable library, not just something that needs to be modified for each application
--- tarot.js
+++ tarot.js
@@ -1,91 +1,39 @@
 /*
-	tarotjs v2 - https://github.com/tylxr59/tarotjs
+	tarotjs v3 - https://github.com/tylxr59/tarotjs
+	Code licensed under MIT License - https://opensource.org/licenses/MIT
 	
-	Takes the numerical placement of the card and returns the location and card
-	By default, putCard writes the image and card in the the element w/ ID of the location/placement
-
 	Rider-Waite-Smith Card Scans by Steve - https://steve-p.org/cards/RWSa.html (High res versions available at his website)
-	
-	Example:
-	<a href="#" onClick="tarotjs('0');" id="0">Pull a Card</a>
 */ 
-const cards = []; // Card storage that exists between generations
+const cards = [];
+
+const majorArcana = ["Fool","Magician","High Priestess","Empress","Emperor","Hierophant","Lovers","Chariot","Strength","Hermit","Wheel of Fortune","Justice","Hanged Man","Death","Temperance","Devil","Tower","Star","Moon","Sun","Judgement","World"];
+const suits = ["Major Arcana", "Cups", "Swords", "Pentacles", "Wands"];
+const numbers = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Page", "Knight", "Queen", "King"];
 
 function tarotjs(location) {
-	const majorArcana = ["The Fool","The Magician","The High Priestess","The Empress","The Emperor","The Hierophant","The Lovers","The Chariot","Strength","The Hermit","Wheel of Fortune","Justice","The Hanged Man","Death","Temperance","The Devil","The Tower","The Star","The Moon","The Sun","Judgement","The World"];
-	
-	var suit = Math.floor(Math.random() * 5); // Generate the suit number
-	
-	if (suit == 0) { // If it's a Major Arcana card...
-		var cardNum = Math.floor(Math.random() * 22); // ...select one of the 22
+	const suitIndex = Math.floor(Math.random() * suits.length);
+	const isMajorArcana = suitIndex === 0;
+	const cardNum = isMajorArcana ? Math.floor(Math.random() * 22) : Math.floor(Math.random() * 14) + 1;
+	const cardID = (suitIndex * 100) + cardNum;
+
+	if (!cards.includes(cardID)) {
+		cards[location] = cardID;
+		const orientation = Math.floor(Math.random() * 2) === 0 ? "Upright" : "Reversed";
+		let cardName = cardNum;
+
+		if (!isMajorArcana) {
+			cardName = numbers[cardNum-1];
+		}
+
+		const theCard = isMajorArcana ? `The ${majorArcana[cardNum]} (${orientation})` : `${cardName} of ${suits[suitIndex]} (${orientation})`;
+		putCard(location, theCard, cardID, orientation);
+		// or return(theCard, cardID, orientation) or something to that affect
 	} else {
-		var cardNum = Math.floor(Math.random() * 14)+1; // Otherwise, generate the regular card number, offset by +1 b/c computers
-	}
-	
-	var cardID = (suit * 100) + cardNum; // Generate the ID of the card to compare against
-	
-	if (!cards.includes(cardID)) { // If card isn't in the array (it hasn't been pulled yet)...
-		cards[location] = cardID; // ...put it in the array...
-		
-		// Select the orientation of the card
-		if (Math.floor(Math.random() * 2) == 0) {
-			var orientation = "Upright";
-		} else {
-			var orientation = "Reversed";
-		}
-		
-		// Face or Ace check (if its not a Major Arcana)
-		if (suit != 0 && (cardNum > 10 || cardNum == 1)) {
-			switch (cardNum) {
-				case 1:
-					cardNum = "Ace";
-					break;
-				case 11:
-					cardNum = "Page";
-					break;
-				case 12:
-					cardNum = "Knight";
-					break;
-				case 13:
-					cardNum = "Queen";
-					break;
-				case 14:
-					cardNum = "King";
-					break;
-			}
-		}
-		
-		// Make it human-readable
-		switch (suit) {
-			case 0:
-				// Major Arcana
-				var theCard = majorArcana[cardNum] + " (" + orientation + ")";
-				break;
-			case 1:
-				// Cups
-				var theCard = cardNum + " of Cups (" + orientation + ")";
-				break;
-			case 2:
-				// Swords
-				var theCard = cardNum + " of Swords (" + orientation + ")";
-				break;
-			case 3:
-				// Pentacles
-				var theCard = cardNum + " of Pentacles (" + orientation + ")";
-				break;
-			case 4:
-				// Wands
-				var theCard = cardNum + " of Wands (" + orientation + ")";
-				break;
-		}
-		
-		putCard(location,theCard,cardID,orientation); // ...and process it
-	} else {
-		tarotjs(location); // Otherwise, try again
+		tarotjs(location);
 	}
 }
 
-// Do something with the card
 function putCard(location, theCard, cardID, orientation) {
-	document.getElementById(location).innerHTML = "<img class=\"" + orientation + "\" src=\"cards/" + cardID + ".jpg\"><br><br>" + theCard;
+	// Do something with the card
+	// Example: document.getElementById(location).innerHTML = `<img class="${orientation.toLowerCase()} card" src="cards/${cardID}.jpg" width="272" height="453"><br><br>${theCard}`;
 }
